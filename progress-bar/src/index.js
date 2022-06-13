20cdf2d371ac2d2324b238e060a1c9229ae6299f
Ayush Singh <ayush.singh.xda@gmail.com>
2022-06-13 20:49:50 +0530

    loader component

--- progress-bar/src/index.js
+++ progress-bar/src/index.js
@@ -1,3 +1,34 @@
 import "./style.css";
 
-(() => {})();
+(() => {
+  const progressBar = document.querySelector(".progress-bar");
+  const progress = document.querySelector(".progress-bar .progress");
+  const triggerBtn = document.querySelector("button");
+
+  const MODES = {
+    PROGRESS: true,
+    INDETERMINATE: false,
+  };
+
+  let currentMode = MODES.INDETERMINATE;
+
+  triggerBtn.addEventListener("click", () => {
+    currentMode = !currentMode;
+
+    switch (currentMode) {
+      case MODES.PROGRESS: {
+        const progressPercent = parseInt(Math.random() * 100);
+        progress.classList.remove("indeterminate");
+        progress.style.setProperty("width", `${progressPercent}%`);
+        progressBar.setAttribute("aria-valuenow", progressPercent);
+        break;
+      }
+      case MODES.INDETERMINATE: {
+        progressBar.removeAttribute("aria-valuenow");
+        progress.classList.add("indeterminate");
+        progress.style.setProperty("width", `100%`);
+        break;
+      }
+    }
+  });
+})();
