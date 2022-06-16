5afd57eb0bc2c7f6542dec518fc3e2a2a3ee0bb7
Ayush Singh <ayush.singh.xda@gmail.com>
2022-06-16 20:15:53 +0530

    checkbox semantics and styles

--- multiselect/src/index.js
+++ multiselect/src/index.js
@@ -1,3 +1,6 @@
 import "./style.css";
 
-(() => {})();
+(() => {
+  const formData = new FormData(document.querySelector("form"));
+  console.warn("Checkboxes", Array.from(formData.entries()));
+})();
