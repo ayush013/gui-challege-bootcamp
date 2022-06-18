a97aa3429db17e44e3af7a8a32a438efec36c02e
Ayush Singh <ayush.singh.xda@gmail.com>
2022-06-18 13:50:43 +0530

    JS handling for arrow keys, selection and menu escape

--- combo-button/src/index.js
+++ combo-button/src/index.js
@@ -1,3 +1,25 @@
 import "./style.css";
 
-(() => {})();
+(() => {
+  const splitButtonGroup = document.querySelector(".button-group");
+
+  splitButtonGroup.addEventListener("click", (event) => {
+    if (event.target.nodeName !== "BUTTON") return;
+    console.log(event.target.innerText);
+  });
+
+  document.addEventListener("keyup", (e) => {
+    const { code, target } = e;
+    if (code === "Escape") {
+      target.blur();
+      return;
+    }
+    if (target.getAttribute("role") === "menuitem") {
+      if (code === "ArrowUp") {
+        target.previousElementSibling?.focus();
+      } else if (code === "ArrowDown") {
+        target.nextElementSibling?.focus();
+      }
+    }
+  });
+})();
