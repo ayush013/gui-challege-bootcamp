1e942056a33a0ad9fce71357bb490acc9969a3c9
Ayush Singh <ayush.singh.xda@gmail.com>
2022-06-18 23:35:38 +0530

    Toggle switch implemented

--- switch/src/index.js
+++ switch/src/index.js
@@ -1,3 +1,17 @@
 import "./style.css";
 
-(() => {})();
+(() => {
+  const switchInput = document.querySelector(".switch input[type=checkbox]");
+  const switchGroup = document.querySelector(".switch");
+
+  switchInput.addEventListener("change", () => {
+    switchGroup.setAttribute("aria-checked", switchInput.checked);
+    switchGroup.classList.toggle("active");
+  });
+
+  switchGroup.addEventListener("keyup", ({ code }) => {
+    if (code === "Space" || code === "Enter") {
+      switchInput.click();
+    }
+  });
+})();
