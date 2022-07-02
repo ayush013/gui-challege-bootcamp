445a36ff21dbfd5f4f9dcbb0d026217df5a542d1
Ayush Singh <ayush.singh.xda@gmail.com>
2022-07-02 14:25:00 +0530

    layouts for event, day and calendar wrapper

--- calendar/src/index.js
+++ calendar/src/index.js
@@ -1,7 +1,34 @@
 import "./style.css";
 
-class Main {
+class Calendar {
   constructor() {}
 }
 
-new Main();
+new Calendar();
+
+// template for weekday cell -> day class
+// week container as section (Calendar)
+// event class -> event cell
+// main class for wiring all together
+// api util with static helper
+
+// DAY BLUEPRINT/CLASS
+// > events, timestamp
+// setter method for events, timestamp
+// setter will have a proxy for layouts
+// takes care of today class
+
+// API UTIL CLASS -> Singleton/static utils
+// static api call method
+// static BASE URL
+
+// EVENT CLASS
+// startTime, endTime, name
+// binds to event template
+
+// CALENDAR CLASS
+// driver for entire application
+// initializeApp -> draws reference to body elements, appends week layout inside body,
+// initializes fetching of event data, adds event listener to buttons
+// set heading for week dates
+// Fetch event callback -> day.setEvents
