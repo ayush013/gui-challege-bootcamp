e3fcb6028014a0a9cd0a7762d472daf1d50a0136
Ayush Singh <ayush.singh.xda@gmail.com>
2022-07-03 17:22:05 +0530

    Low level design | api util

---
+++ search-filter/src/app/ApiUtil.js
@@ -0,0 +1,15 @@
+const API_KEY = "3RxA3kiBwdUQ5S2X6IzXpLJoLACjzhr1";
+
+export default class ApiUtil {
+  static searchEndpoint = "https://api.giphy.com/v1/gifs/search";
+
+  constructor() {}
+
+  static async getSearchResults(query, offset = 0, limit = 20) {
+    const response = await fetch(
+      `${ApiUtil.searchEndpoint}?q=${query}&offset=${offset}&limit=${limit}&api_key=${API_KEY}`
+    );
+    const data = await response.json();
+    return data;
+  }
+}
--- search-filter/src/index.js
+++ search-filter/src/index.js
@@ -5,3 +5,47 @@ class Main {
 }
 
 new Main();
+
+// Api Util with API fetch
+// Store with data and page number
+// search component with event on change
+// tile component with props
+// main class with driver logic
+// intersection observer class with onchange callback
+
+// API UTIL (DUMB)
+// static API fetch method
+
+// STORE (PROVIDER)
+// data from API
+// loading and error states
+// pagination -> page number
+// method to subscribe to store updates
+
+// SEARCH (PROVIDER)
+// binds to template of search bar
+// emits on change event with debounce
+
+// TILE COMPONENT (CONSUMER)
+// props -> title, desc, user avatar
+
+// INTERSECTION OBSERVER (PROVIDER)
+// props -> element to observe
+// onViewabilityChanged callback
+
+// MAIN
+// initialize search component
+// hook to search changes
+
+// methodX
+// get pagination data from store
+// make API call
+// update store state loading
+// update store data on fetch
+
+// subscription to store data
+// callback with render template logic
+
+// intersection oberver to listen to load more element
+// listens to viewability change
+// calls methodX
