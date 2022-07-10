a20ea999ae5b552e5c40d5e61763a9c17aa9c080
Ayush Singh <ayush.singh.xda@gmail.com>
2022-07-10 14:16:49 +0530

    toast animation refactor

--- toast/src/app/Toast.js
+++ toast/src/app/Toast.js
@@ -2,7 +2,7 @@ class Toast {
   constructor() {
     this.template = document.getElementById("toast");
     this.parentContainer = document.querySelector(".toast-container");
-    this.timeout = 5000;
+    this.timeout = 3000;
 
     this.create();
   }
@@ -17,26 +17,22 @@ class Toast {
     setTimeout(this.dismissToast, this.timeout);
   }
 
-  animateFadeOutToast = () => {
-    return new Promise((resolve) => {
-      this.toast.animate(
-        [
-          {
-            opacity: 0,
-            transform: "translateY(-50px)",
-          },
-        ],
-        { duration: 300, easing: "ease", fill: "forwards" }
-      );
-      setTimeout(resolve, 300);
-    });
+  animateToastDismiss = () => {
+    this.toast.animate(
+      [
+        {
+          opacity: 0,
+          transform: "translateY(-50px)",
+        },
+      ],
+      { duration: 300, easing: "ease", fill: "forwards" }
+    );
   };
 
   dismissToast = () => {
-    this.animateFadeOutToast().then(() => {
-      this.listenToastAnimationEnd(toast).then(() => {
-        this.toast.remove();
-      });
+    this.animateToastDismiss();
+    this.listenToastAnimationEnd().then(() => {
+      this.toast.remove();
     });
   };
 
@@ -54,8 +50,8 @@ class Toast {
     return text.length > 10 && text.length < 50 ? text : this.getRandomText();
   }
 
-  async listenToastAnimationEnd(toast) {
-    const animationPromises = toast.getAnimations().map((a) => a.finished);
+  async listenToastAnimationEnd() {
+    const animationPromises = this.toast.getAnimations().map((a) => a.finished);
 
     return await Promise.allSettled(animationPromises);
   }
